3e5b0ec9a00dc7ad76a125cad30e8b5ea8db3f13
hamza0001 <elaoutarhamza@gmail.com>
2018-02-11 21:51:23 +0000

    added firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCVcQPOpE5lA635O5DqsUYQJ99oWp4aOBA",
+    authDomain: "pwd-manage.firebaseapp.com",
+    databaseURL: "https://pwd-manage.firebaseio.com",
+    projectId: "pwd-manage",
+    storageBucket: "pwd-manage.appspot.com",
+    messagingSenderId: "783325120173",
+};
+
+firebase.initializeApp(config);
