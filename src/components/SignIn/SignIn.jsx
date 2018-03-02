7188d1d66f04cf0183e8ff5ff2663df83fa09fe2
hamza <elaoutarhamza@gmail.com>
2018-03-02 16:42:49 +0000

    added SignIn specs file

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './SignIn.less';
 
-const SignIn = ({emailRef, passwordRef, submitHandler}) => (
+const SignIn = ({ emailRef, passwordRef, submitHandler, fbAuthHandler, twAuthHandler }) => (
     <div className="main">
         <div className="social-icons">
             <div className="col_1_of_f span_1_of_f">
-                <a href="#">
+                <a onClick={fbAuthHandler} href="#">
                     <ul className='facebook'>
                         <i className="fb"> </i>
                         <li>Connect with Facebook</li>
                         <div className='clear'> </div>
                     </ul>
                 </a>
-            </div>	
+            </div>
             <div className="col_1_of_f span_1_of_f">
-                <a href="#">
+                <a onClick={twAuthHandler} href="#">
                     <ul className='twitter'>
                         <i className="tw"> </i>
                         <li>Connect with Twitter</li>
@@ -22,19 +22,19 @@ const SignIn = ({emailRef, passwordRef, submitHandler}) => (
                     </ul>
                 </a>
             </div>
-            <div className="clear"> </div>	
+            <div className="clear"> </div>
         </div>
         <h2>Or SignIn with your email/password</h2>
-        <form>
+        <div>
             <div className="lable-2">
                 <input type="text" className="text" ref={emailRef} />
-                <input type="password" className="text" ref={passwordRef}/>
+                <input type="password" className="text" ref={passwordRef} />
             </div>
             <div className="submit">
                 <input type="submit" onClick={submitHandler} value="Sign In" />
             </div>
             <div className="clear"> </div>
-        </form>
+        </div>
     </div>
 );
 
---
+++ src/components/SignIn/SignInSpec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+describe('SignIn', () => {
+    const props = {
+        emailRef: jest.fn(),
+        passwordRef: jest.fn(),
+        submitHandler: jest.fn(),
+        fbAuthHandler: jest.fn(),
+        twAuthHandler: jest.fn()
+    };
+
+    it('', () => {
+
+    });
+});
