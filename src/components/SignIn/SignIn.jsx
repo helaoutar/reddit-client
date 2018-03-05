4839b81b71d6be3ad01a64e3e619098a88ceafe6
hamza <elaoutarhamza@gmail.com>
2018-03-05 14:10:51 +0000

    added eslint

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -6,19 +6,19 @@ const SignIn = ({ emailRef, passwordRef, submitHandler, fbAuthHandler, twAuthHan
         <div className="social-icons">
             <div className="col_1_of_f span_1_of_f">
                 <a onClick={fbAuthHandler} href="#">
-                    <ul className='facebook'>
+                    <ul className="facebook">
                         <i className="fb"> </i>
                         <li>Connect with Facebook</li>
-                        <div className='clear'> </div>
+                        <div className="clear"> </div>
                     </ul>
                 </a>
             </div>
             <div className="col_1_of_f span_1_of_f">
                 <a onClick={twAuthHandler} href="#">
-                    <ul className='twitter'>
+                    <ul className="twitter">
                         <i className="tw"> </i>
                         <li>Connect with Twitter</li>
-                        <div className='clear'> </div>
+                        <div className="clear"> </div>
                     </ul>
                 </a>
             </div>
