78128ded5205300112914a43166396838f0ee551
hamza <elaoutarhamza@gmail.com>
2018-03-29 11:16:37 +0100

    Added signIn logo

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -3,18 +3,23 @@ import React from 'react';
 import Loader from '../Loader';
 
 import './SignIn.less';
+import logo from './reddit.png';
 
 const SignIn = ({
         emailRef,
         passwordRef,
         submitHandler,
-        isLoading
+        isLoading,
+        logoSrc
     }) => (
     <div className="SignIn">
+        <div className="SignIn__logo-container">
+            <img src={logo} className="SignIn__logo"/>
+        </div>
         <div className="SignIn__form">
-            <input className="SignIn__email" ref={ emailRef } placeholder="email"/>
-            <input className="SignIn__password" type="password" ref={ passwordRef } placeholder="password" />
-            <input type="submit" className="SignIn__submit-button" />
+            <input className="SignIn__email" type="email" ref={emailRef} placeholder="email"/>
+            <input className="SignIn__password" type="password" ref={passwordRef} placeholder="password" />
+            <input type="submit" className="SignIn__submit-button" onClick={submitHandler}/>
         </div>
     </div>
 );
--- src/reducers/signInErros.js
+++ src/reducers/signInErros.js
@@ -1,9 +1,11 @@
-import { FAILED_SIGN_IN } from '../actions';
+import { FAILED_SIGN_IN, SUCCESS_SIGN_IN } from '../actions';
 
 export default (state = [], { type, errors }) => {
     switch (type) {
     case FAILED_SIGN_IN:
         return errors;
+    case SUCCESS_SIGN_IN:
+        return "";
     default:
         return state;
     }
